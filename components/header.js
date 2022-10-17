fe4c0c0ba93fe5e326a757f195261283cf62b062
Paul Frazee <pfrazee@gmail.com>
2022-10-17 15:48:24 -0500

    Move developer links behind dev dropdown to improve clarity

--- components/header.js
+++ components/header.js
@@ -10,6 +10,7 @@ import {
 } from '@heroicons/react/24/outline'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
 
 const SiteSections = [
   {
@@ -46,11 +47,27 @@ const ExternalLinks = [
   },
 ]
 
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+/*
+{SiteSections.map((item) => (
+  <Link key={item.href} href={item.href}>
+    <a
+      title={item.name}
+      className="text-base font-medium text-gray-500 hover:text-gray-900"
+    >
+      {item.name}
+    </a>
+  </Link>
+))}*/
+
 export default function Header() {
   return (
     <Popover className="relative z-[20] bg-white">
       <div className="flex items-center justify-between border-b-2 border-gray-100 px-4 sm:px-6 py-4 sm:py-6 md:justify-start md:space-x-10">
-        <div className="flex justify-start lg:w-0 lg:flex-1">
+        <div className="flex justify-start">
           <Link href="/">
             <a>
               <span className="sr-only">ATP</span>
@@ -65,16 +82,64 @@ export default function Header() {
           </Popover.Button>
         </div>
         <nav className="hidden space-x-10 md:flex">
-          {SiteSections.map((item) => (
-            <Link key={item.href} href={item.href}>
-              <a
-                title={item.name}
-                className="text-base font-medium text-gray-500 hover:text-gray-900"
-              >
-                {item.name}
-              </a>
-            </Link>
-          ))}
+          <Popover className="relative">
+            {({ open }) => (
+              <>
+                <Popover.Button
+                  className={classNames(
+                    open ? 'text-gray-900' : 'text-gray-500',
+                    'group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
+                  )}
+                >
+                  <span>Developers</span>
+                  <ChevronDownIcon
+                    className={classNames(
+                      open ? 'text-gray-600' : 'text-gray-400',
+                      'ml-2 h-5 w-5 group-hover:text-gray-500'
+                    )}
+                    aria-hidden="true"
+                  />
+                </Popover.Button>
+
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-200"
+                  enterFrom="opacity-0 translate-y-1"
+                  enterTo="opacity-100 translate-y-0"
+                  leave="transition ease-in duration-150"
+                  leaveFrom="opacity-100 translate-y-0"
+                  leaveTo="opacity-0 translate-y-1"
+                >
+                  <Popover.Panel className="absolute left-1/2 z-10 mt-3 w-screen max-w-xs -translate-x-1/2 transform px-2 sm:px-0">
+                    <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                      <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                        {SiteSections.map((item) => (
+                          <a
+                            key={item.name}
+                            href={item.href}
+                            className="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50"
+                          >
+                            <item.icon
+                              className="h-6 w-6 flex-shrink-0 text-blue-600"
+                              aria-hidden="true"
+                            />
+                            <div className="ml-4">
+                              <p className="text-base font-medium text-gray-900">
+                                {item.name}
+                              </p>
+                              <p className="mt-1 text-sm text-gray-500">
+                                {item.description}
+                              </p>
+                            </div>
+                          </a>
+                        ))}
+                      </div>
+                    </div>
+                  </Popover.Panel>
+                </Transition>
+              </>
+            )}
+          </Popover>
         </nav>
         <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
           {ExternalLinks.map((item) => (
