20337134060d070ce01684c6825b2adc02b7b2ba
ttrreevvoorr <trevormaustin@pm.me>
2023-04-23 21:56:31 -0700

    Change structure of <Link> and applies visible focus on /pages/docs

--- pages/docs.js
+++ pages/docs.js
@@ -25,31 +25,31 @@ export default function Docs({ navigation }) {
 
       <div className="max-w-4xl mx-auto px-4">
         <h3 className="text-3xl mb-2 font-normal">Guides</h3>
-        <div className="relative flex px-6 py-6 border border-gray-300 rounded-xl mb-4 hover:border-blue-500">
-          <div className="mr-4 hidden sm:block">
-            <span className="bg-blue-500 text-white rounded-lg inline-flex p-2 sm:p-3 ring-4 ring-white">
-              <AcademicCapIcon
-                className="h-8 sm:h-10 w-8 sm:w-10"
-                aria-hidden="true"
-              />
-            </span>
-          </div>
-          <div className="pt-1.5">
-            <h3 className="text-2xl font-bold leading-6 mb-1">
-              <Link href={firstPage.href}>
-                <a className="focus:outline-none">
-                  {/* Extend touch target to entire panel */}
-                  <span className="absolute inset-0" aria-hidden="true" />
-                  {firstPage.name}{' '}
-                  <span className="bg-blue-100 text-blue-700 font-mono text-xs px-1.5 pt-0.5 pb-0.5 rounded-md -top-0.5 relative">
-                    Start&nbsp;here
-                  </span>
-                </a>
-              </Link>
-            </h3>
-            <p className="text-md">{firstPage.description}</p>
-          </div>
-        </div>
+
+        <Link href={firstPage.href}>
+          <a className="relative flex px-6 py-6 border border-gray-300 rounded-xl mb-4 w-full focus:outline-none focus:border-blue-500 hover:border-blue-500">
+            <div className="mr-4 hidden sm:block">
+              <span className="bg-blue-500 text-white rounded-lg inline-flex p-2 sm:p-3 ring-4 ring-white">
+                <AcademicCapIcon
+                  className="h-8 sm:h-10 w-8 sm:w-10"
+                  aria-hidden="true"
+                />
+              </span>
+            </div>
+            <div className="pt-1.5">
+              <h3 className="text-2xl font-bold leading-6 mb-1">
+                {/* Extend touch target to entire panel */}
+                <span className="absolute inset-0" aria-hidden="true" />
+                {firstPage.name}{" "}
+                <span className="bg-blue-100 text-blue-700 font-mono text-xs px-1.5 pt-0.5 pb-0.5 rounded-md -top-0.5 relative">
+                  Start&nbsp;here
+                </span>
+              </h3>
+              <p className="text-md">{firstPage.description}</p>
+            </div>
+          </a>
+        </Link>
+
         <Links pages={navigation.guides.slice(1)} />
 
         <h3 className="text-3xl mb-2 font-normal">Specs</h3>
@@ -67,29 +67,26 @@ function Links({ pages }) {
   return (
     <div className="mb-8">
       {pages.map((page, pageIdx) => (
-        <div
-          key={page.href}
-          className="relative flex items-center px-6 py-6 border border-gray-300 rounded-xl mb-4 hover:border-blue-500"
-        >
-          <div className="flex-1">
-            <h3 className="text-xl leading-6 font-light">
-              <Link href={page.href}>
-                <a className="focus:outline-none">
-                  {/* Extend touch target to entire panel */}
-                  <span className="absolute inset-0" aria-hidden="true" />
-                  {page.name}
-                </a>
-              </Link>
-            </h3>
-            <p className="text-md text-gray-500 font-light">
-              {page.description}
-            </p>
-          </div>
-          <ChevronRightIcon
-            className="h-8 sm:h-10 w-8 sm:w-10"
-            aria-hidden="true"
-          />
-        </div>
+        <Link key={page.href} href={page.href}>
+          <a
+            className="relative flex items-center px-6 py-6 border border-gray-300 rounded-xl mb-4 focus:outline-none focus:border-blue-500 hover:border-blue-500 block text-left w-full"
+          >
+            <div className="flex-1">
+              <h3 className="text-xl leading-6 font-light">
+                {/* Extend touch target to entire panel */}
+                <span className="absolute inset-0" aria-hidden="true" />
+                {page.name}
+              </h3>
+              <p className="text-md text-gray-500 font-light">
+                {page.description}
+              </p>
+            </div>
+            <ChevronRightIcon
+              className="h-8 sm:h-10 w-8 sm:w-10"
+              aria-hidden="true"
+            />
+          </a>
+        </Link>
       ))}
     </div>
   )
