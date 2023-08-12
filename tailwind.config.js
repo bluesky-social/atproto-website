dce63b73e279c6d9adf077b831cada0f8879980e
liberaldev <ssharp0614@gmail.com>
2023-08-12 10:27:22 +0900

    Remove system-ui from the font-family

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["-apple-system", "BlinkMacSystemFont", "\"Segoe UI\"", "Roboto", "\"Helvetica Neue\"", "Arial", "\"Noto Sans\"", "sans-serif", "\"Apple Color Emoji\"", "\"Segoe UI Emoji\"", "\"Segoe UI Symbol\"", "\"Noto Color Emoji\""],
+      }
+    },
   },
   plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
 }
