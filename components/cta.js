4614debb2c9fef7a16dbaceef05115c21d9970cc
bryan newbold <bnewbold@robocracy.org>
2023-09-14 15:02:21 -0700

    homepage: promote sandbox

--- components/cta.js
+++ components/cta.js
@@ -5,15 +5,20 @@ export default function CTA() {
     <div className="bg-blue-700">
       <div className="mx-auto max-w-2xl py-16 px-4 text-center sm:py-20 sm:px-6 lg:px-8">
         <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-          <span className="block">Join the Bluesky private beta.</span>
+          <span className="block">Join the Federated Sandbox</span>
         </h2>
         <p className="mt-4 text-lg leading-6 text-blue-200">
-          The AT Protocol will launch soon.
+          No waitlist or invite code required. The production network will open soon.
           <br />
         </p>
-        <Link href="https://bsky.app">
+        <Link href="/blog/federation-developer-sandbox">
           <a className="mt-8 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-blue-600 hover:bg-blue-50 focus-visible:outline outline-yellow-500 outline-2 sm:w-auto">
-            Join the waitlist
+            Sandbox Guidelines
+          </a>
+        </Link>
+        <Link href="https://github.com/bluesky-social/pds">
+          <a className="mt-8 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-blue-600 hover:bg-blue-50 focus-visible:outline outline-yellow-500 outline-2 sm:w-auto">
+            Self-Hosting
           </a>
         </Link>
       </div>
