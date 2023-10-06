95e8fe43d13411e6a639d3fe97d3c7c716ef005e
Emily Liu <emilyliu732l@gmail.com>
2023-10-06 12:18:55 -0700

    update endpoint

--- components/mailing-list.js
+++ components/mailing-list.js
@@ -7,7 +7,7 @@ export default function DevMailingListSubscription({ isFooter }) {
     <div className={containerClass}>
       {!isFooter && <hr className="dev-subscription-bar"></hr>}
       <h3>To receive atproto updates in your inbox, subscribe to the developer mailing list.</h3>
-      <form action="https://api.mailmodo.com/api/v1/at/f/-Ds0vhHmIT/bee5e09c-9924-5634-9dc1-16922d543ef3" method="post">
+      <form action="https://api.mailmodo.com/api/v1/at/f/lQtwgpiir6/1b8faade-4671-5d62-9c1c-6cf4063b2bd4" method="post">
         <input className={inputClass} type="email" name="email" placeholder="Your email" required />
         <button className="subscribe-button" type="submit">Subscribe</button>
       </form>
