816365485f52ae2243f5795af7065aac6290b370
Tom VanAntwerp <tom@tomvanantwerp.com>
2023-12-27 11:26:33 -0500

    Add system data-theme to html to enable search dark mode
    DocSearch's dark mode can only be enabled by setting [data-theme="dark"] on a parent element.

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
+import { useEffect } from 'react';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+    if (systemTheme) {
+      document.documentElement.setAttribute('data-theme', systemTheme)
+    }
+  }, [])
+
   return <Component {...pageProps} />
 }
 
