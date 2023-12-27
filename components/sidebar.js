1bcec804089606ab90a979777690cdfd9aa6ac65
Tom VanAntwerp <tom@tomvanantwerp.com>
2023-12-27 09:51:23 -0500

    Add dark mode colors to sidebar

--- components/sidebar.js
+++ components/sidebar.js
@@ -6,7 +6,7 @@ function classNames(...classes) {
 
 export function Sidebar({ navigation, current }) {
   return  (
-    <div className="hidden relative lg:block bg-white pb-4 w-[208px] rounded-lg">
+    <div className="hidden relative lg:block bg-default pb-4 w-[208px] rounded-lg">
       <nav className="sticky top-[10px] px-2 overflow-y-scroll h-vh pb-4 max-h-[98vh]" aria-label="Sidebar">
         <h4 className="font-bold text-sm mb-1">Guides</h4>
         <LinksGroup links={navigation.guides} current={current} />
@@ -21,7 +21,7 @@ export function Sidebar({ navigation, current }) {
 
 export function CommunitySidebar({ navigation, current }) {
   return  (
-    <div className="hidden relative lg:block bg-white pb-4 w-[208px] rounded-lg">
+    <div className="hidden relative lg:block bg-default pb-4 w-[208px] rounded-lg">
       <nav className="sticky top-[10px] px-2" aria-label="Sidebar">
         <h4 className="font-bold text-sm mb-1">Community</h4>
         <LinksGroup links={navigation.community} current={current} />
@@ -32,7 +32,7 @@ export function CommunitySidebar({ navigation, current }) {
 
 export function BlogSidebar({ navigation, current }) {
   return  (
-    <div className="hidden relative lg:block bg-white pb-4 w-[208px] rounded-lg">
+    <div className="hidden relative lg:block bg-default pb-4 w-[208px] rounded-lg">
       <nav className="sticky top-[10px] px-2" aria-label="Sidebar">
         <h4 className="font-bold text-sm mb-1">Dev Blog</h4>
         <LinksGroup links={navigation.blog} current={current} />
@@ -49,8 +49,8 @@ function LinksGroup({ links, current }) {
           <a
             className={classNames(
               item.href === current
-                ? 'bg-gray-100 text-gray-900'
-                : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
+                ? 'bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-100'
+                : 'text-gray-600 dark:text-gray-300 hover:bg-gray-50 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-gray-100',
               'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
             )}
           >
