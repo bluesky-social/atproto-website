df7384adcc5f08cd3044cf4f765cc3122cab86e7
Tom VanAntwerp <tom@tomvanantwerp.com>
2023-12-27 13:39:20 -0500

    Add dark colors to TLDR component

--- components/tldr.js
+++ components/tldr.js
@@ -2,18 +2,18 @@ import { ArrowRightCircleIcon } from '@heroicons/react/24/solid'
 
 export default function TLDR({ items }) {
   return (
-    <div className="bg-gray-50 rounded mb-8 mt-2 lg:mt-0">
+    <div className="bg-gray-50 dark:bg-gray-900 rounded mb-8 mt-2 lg:mt-0">
       <h3 className="font-bold px-4 py-2">TL;DR:</h3>
       {items.map((item, i) => (
         <div
           key={i}
-          className="flex items-start px-4 py-2 border-t border-gray-100"
+          className="flex items-start px-4 py-2 border-t border-gray-100 dark:border-gray-800"
         >
           <ArrowRightCircleIcon
             className="h-5 w-5 text-blue-500"
             aria-hidden="true"
           />
-          <div className="ml-2 flex-1 text-gray-600 text-sm">{item}</div>
+          <div className="ml-2 flex-1 text-gray-600 dark:text-gray-300 text-sm">{item}</div>
         </div>
       ))}
     </div>
