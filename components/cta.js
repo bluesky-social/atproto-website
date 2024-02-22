310184c3ef12e45c6e9dc29327fb2d0984c89e3e
Emily Liu <emilyliu7321@gmail.com>
2024-02-22 13:18:37 -0800

    Update federation cta

--- components/cta.js
+++ components/cta.js
@@ -5,13 +5,13 @@ export default function CTA() {
     <div className="bg-blue-700">
       <div className="mx-auto max-w-2xl py-16 px-4 text-center sm:py-20 sm:px-6 lg:px-8">
         <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-          <span className="block">Join the Federated Sandbox</span>
+          <span className="block">Join the Federated Network</span>
         </h2>
         <p className="mt-4 text-lg leading-6 text-blue-200">
-          No invite code required to join. The production network will open to federation soon.
+          Participate in early access federation for self-hosters.
           <br />
         </p>
-        <Link href="/blog/federation-developer-sandbox">
+        <Link href="https://docs.bsky.app/blog/self-host-federation">
           <a className="mt-8 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-blue-600 hover:bg-blue-50 focus-visible:outline outline-yellow-500 outline-2 sm:w-auto">
             Guidelines and Instructions
           </a>
