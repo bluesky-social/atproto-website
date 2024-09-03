e59e1b1cfb2b037951ed33e8a8301d869b01eb38
Paul Frazee <pfrazee@gmail.com>
2024-09-03 14:59:57 -0700

    Add PT translations of all guides and articles

---
+++ src/app/[locale]/guides/overview/page.tsx
@@ -0,0 +1,9 @@
+export default async function HomePage({ params }: any) {
+  try {
+    const Content = (await import(`./${params.locale}.mdx`)).default
+    return <Content />
+  } catch (error) {
+    const Content = (await import(`./en.mdx`)).default
+    return <Content />
+  }
+}
