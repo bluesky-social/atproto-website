f1cfc0c95d11841fda890ed8cdb6c290c537bd1e
Jang Jinsu <huke@lsware.com>
2025-02-16 22:15:39 +0900

    Add korean translations

---
+++ src/app/[locale]/specs/sync/page.tsx
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: 'Sync',
+  description: 'Firehose and other data synchronization mechanisms.',
+}
+
+export default async function HomePage({ params }: any) {
+  try {
+    const Content = (await import(`./${params.locale}.mdx`)).default
+    return <Content />
+  } catch (error) {
+    const Content = (await import(`./en.mdx`)).default
+    return <Content />
+  }
+}
