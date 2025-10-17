3a521d92b7d3da8e5a4bd459ed5164f78a48b82f
David Buchanan <david@blueskyweb.xyz>
2025-10-17 15:12:26 +0100

    add oauth introduction guide

---
+++ src/app/[locale]/guides/oauth/page.tsx
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: 'OAuth Introduction',
+  description: 'OAuth for AT Protocol application developers.',
+}
+
+export default async function HomePage({ params }: any) {
+  try {
+    const Content = (await import(`./${params.locale}.mdx`)).default
+    return <Content />
+  } catch (error) {
+    const Content = (await import(`./en.mdx`)).default
+    return <Content />
+  }
+}
