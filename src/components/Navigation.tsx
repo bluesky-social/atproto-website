050f4513fb10b4b04e189f319783ffc542a82114
Alex Garnett <axfelix@gmail.com>
2025-12-16 11:54:24 -0800

    hacking on permission sets guide

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -265,6 +265,7 @@ export const navigation: Array<NavGroup> = [
       { title: 'PDS Self-Hosting', href: '/guides/self-hosting' },
       { title: 'Going to production', href: '/guides/going-to-production' },
       { title: 'OAuth Introduction', href: '/guides/oauth' },
+      { title: 'Permission Sets', href: '/guides/permission-sets' },
       { title: 'Account Migration', href: '/guides/account-migration' },
     ],
   },
