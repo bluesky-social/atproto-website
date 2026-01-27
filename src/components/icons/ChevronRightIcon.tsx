5db98fb55a8a45c8a09495937b3b0518f0067b07
Paul Frazee <pfrazee@gmail.com>
2026-01-26 17:27:32 -0800

    Typo fix

--- src/components/icons/ChevronRightIcon.tsx
+++ src/components/icons/ChevronRightIcon.tsx
@@ -2,9 +2,9 @@ export function ChevronRightIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
   return (
     <svg viewBox="0 0 16 16" aria-hidden="true" {...props}>
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M6.22 4.22a.75.75 0 0 1 1.06 0l3.25 3.25a.75.75 0 0 1 0 1.06l-3.25 3.25a.75.75 0 0 1-1.06-1.06L8.94 8 6.22 5.28a.75.75 0 0 1 0-1.06Z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       />
     </svg>
   )
