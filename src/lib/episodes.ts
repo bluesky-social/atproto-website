2591db0dca32896322499dbad3a9657cdd276639
Jim Ray <jimray@gmail.com>
2026-06-25 22:05:44 -0400

    Fix for errant episodes data

--- src/lib/episodes.ts
+++ src/lib/episodes.ts
@@ -104,7 +104,6 @@ export const episodes: Episode[] = [
     pubDate: '2026-06-26T01:16:18.447Z',
     duration: '00:46:35',
     durationSeconds: 2794,
-    hosts: ['Jim Ray', 'Alex Garnett'],
     guests: ['Daniel Holmgren'],
     audioUrl: 'https://media.atproto.com/off-protocol/2026-06-24-ama-dholms/2020-06-24-dholms-ama.mp3',
     audioSizeBytes: 22464670,
