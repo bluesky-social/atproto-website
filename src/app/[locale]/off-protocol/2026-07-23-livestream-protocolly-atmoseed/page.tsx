13a6bea38cd18d5411468b6e738dbc8c18ec11ef
Jim Ray <470581+jimray@users.noreply.github.com>
2026-07-23 13:28:27 -0400

    off protocol 2026 07 23 (#717)
    * Added 07-23 livestream
    
    * Episode edited

---
+++ src/app/[locale]/off-protocol/2026-07-23-livestream-protocolly-atmoseed/page.tsx
@@ -0,0 +1,26 @@
+import { EpisodePage } from '@/components/EpisodePage'
+
+export const metadata = {
+  title: 'Protocolly Atmoseed',
+  description: 'News from around the Atmosphere, including a proposal to fix localhost, more webdevs getting into atproto, Mu Social\'s opinionated news feed, and more.',
+}
+
+export default async function EpisodeRoute({ params }: any) {
+  const Notes = await import(`./${(await params).locale}.mdx`).catch(
+    () => import(`./en.mdx`),
+  )
+  let Transcript = null
+  try {
+    Transcript = await import(`./transcript.mdx`)
+  } catch {
+    // optional
+  }
+
+  return (
+    <EpisodePage
+      default={Notes.default}
+      header={Notes.header}
+      Transcript={Transcript?.default}
+    />
+  )
+}
